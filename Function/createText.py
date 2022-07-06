f991d6ba815242c4267f45244be0ee453b0c1580
lbcmk <30442978+lbcmk@users.noreply.github.com>
2022-07-06 17:58:34 -0400

    Made a Main Menu

---
+++ Function/createText.py
@@ -0,0 +1,14 @@
+from pygame import font
+
+def createText(X, Y, size, textColour, weight, textInfo, alignment = "l"):
+        fontx = font.Font(f"./Font/Lato-{weight}.ttf", size)
+        text = fontx.render(textInfo, True, textColour)
+        textRect = text.get_rect()
+        if(alignment == "left" or alignment == "l"):
+            textRect = (X // 2, Y // 2) # Left aligned text
+        elif(alignment == "center" or alignment == "c"):
+            textRect.center = (X//2, Y//2) # Centered text
+        elif(alignment == "right" or alignment == "r"):
+            textRect.right = (X//2, Y//2) # Right aligned text
+
+        return text, textRect;
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from Setup import *
 from Game import Game
-
-scene = Game()
+from Menu import Menu
 
 delta = 1000//fps
 is_running = True
+
+level = 0
+old_level = level
+scene = Menu()
+
 while is_running:
     if pg.event.peek(pg.QUIT):
         is_running = False
+
+    if(level == 0):
+        level = scene.level
+
+    if(old_level != level):
+        old_level = level
+        match level:
+            case 0:
+                scene = Menu()
+            case 1:
+                scene = Game()
 
     scene.update(delta)
     scene.draw(screen)
