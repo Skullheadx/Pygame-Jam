c32fbf835b1d0ed8a85fb9172172f3d98352ea04
Skullheadx <704277@pdsb.net>
2022-07-05 20:15:45 -0400

    added main loop

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('Hello World')
+import pygame as pg
+
+
+pg.init()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 1080, 640
+dimensions = (SCREEN_WIDTH, SCREEN_HEIGHT)
+center = pg.Vector2(dimensions) / 2
+
+pg.display.set_caption("Jam")
+# icon = pg.transform.scale(pg.image.load("logo.ico"), (32, 32))
+# pg.display.set_icon(icon)
+
+clock = pg.time.Clock()
+fps = 60
+
+screen = pg.display.set_mode(dimensions, pg.SCALED)
+
+
+delta = 1000//fps
+
+is_running = True
+while is_running:
+    screen.fill((255,255,255))
+
+    if pg.event.peek(pg.QUIT, pump=True):
+        is_running = False
+
+    pg.display.update()
+    delta = clock.tick(fps)
+
+pg.quit()
