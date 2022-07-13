4f2aa41d500f397ee9b3727f5e3bbb0ef9937d4d
711215 <711215@pdsb.net>
2022-07-13 17:01:22 -0400

    hidden area in level 1

--- Pet.py
+++ Pet.py
@@ -1,3 +1,4 @@
+from Setup import *
 from Actors import Actor
 from Player import Player
 
@@ -5,6 +6,12 @@ class Pet(Actor):
     width,height = 35,25
     speed = Player.speed * 0.75
     jump_strength = Player.jump_strength / 2
+
+    move_gif = Image.open("Assets/SNAKE.gif")
+    move_frames = []
+    #for i in range(move_gif.n_frames):
+        #idk how this works
+        #move_frames.append(pg.transform.scale(pil_to_game(get_gif_frame(move_gif, i)), (200, 200)))
 
     def __init__(self, pos, collision_layer, collision_mask):
         super().__init__(pos, collision_layer, collision_mask)
